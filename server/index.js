4a79889e89841a735931f2eb7857c36f4a5b8e71
JackCT1 <jackthomas933@gmail.com>
2023-07-05 23:27:04 +0100

    using controllers for get request

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from "express";
 
+import { getPosts } from "../controllers/posts";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Server response");
-});
+router.get("/", getPosts);
 
 export default router;
