ac17e9e4277beab0e00df627686cdd05e3d8d49c
JackCT1 <jackthomas933@gmail.com>
2023-07-06 23:30:46 +0100

    get request finished

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,11 @@ import PostMessage from "../models/postMessage";
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-  } catch (error) {}
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 export const createPost = (req, res) => {
