72e558b9f7221612ee96c2f345c471eabd8d64c5
JackCT1 <jackthomas933@gmail.com>
2023-07-10 21:43:56 +0100

    app container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,14 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          The Jack Thomas App
+        </Typography>
+        <img src={} alt="Logo" height="60"/>
+      </AppBar>
+    </Container>
   );
 };
 
