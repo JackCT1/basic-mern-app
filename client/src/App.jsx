bc73cbb04e45ca32864e3dfeeda9d8ec722e4fe2
JackCT1 <jackthomas933@gmail.com>
2023-07-12 23:23:42 +0100

    styles for form and post components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-import Posts from "../components/Posts/Posts";
-import Form from "../components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import memories from "./images/memories.png";
 import useStyles from "./styles";
 
