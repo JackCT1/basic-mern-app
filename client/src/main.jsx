2f77346ac6e661af46d5cf96c97cb1aec1969ec5
JackCT1 <jackthomas933@gmail.com>
2023-07-13 23:58:12 +0100

    using redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,16 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import reducers from "./reducers";
 
+import reducers from "./reducers";
 import App from "./App.jsx";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
