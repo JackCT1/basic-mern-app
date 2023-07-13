3bc365c29945bff0bdabb91a6aee36b5c9960748
JackCT1 <jackthomas933@gmail.com>
2023-07-13 23:48:14 +0100

    beginning reducer functions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+import App from "./App.jsx";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
