a3b3fceae712fcf68c6da96ed74748690d4900a2
JackCT1 <jackthomas933@gmail.com>
2023-07-14 17:44:01 +0100

    fixed network bug

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ dotenv.config();
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3000;
