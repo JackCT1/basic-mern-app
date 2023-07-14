28e905a9b31efea102b404fc0ba0722be4a090b1
JackCT1 <jackthomas933@gmail.com>
2023-07-14 15:17:35 +0100

    store dependency resolved, content rendering again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
 import reducers from "./reducers";
 import App from "./App.jsx";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = configureStore(
+  { reducer: reducers },
+  compose(applyMiddleware(thunk))
+);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
