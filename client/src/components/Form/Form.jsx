c7e9e5c87bbf545ee073aa7dd450118f81800fa0
JackCT1 <jackthomas933@gmail.com>
2023-07-16 15:48:06 +0100

    dispatching action upon submit

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import useStyles from "./styles";
 import FileBase from "react-file-base64";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -12,8 +14,12 @@ const Form = () => {
     selectedFile: "",
   });
   const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData));
+  };
 
   const clear = () => {};
   return (
