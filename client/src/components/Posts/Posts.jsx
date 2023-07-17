784e738f831961bd6bbe2c3c40d4ca588c997210
JackCT1 <jackthomas933@gmail.com>
2023-07-17 18:49:09 +0100

    posts grid

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,4 +1,5 @@
 import Post from "./Post/Post";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import useStyles from "./styles";
 
@@ -8,11 +9,21 @@ const Posts = () => {
   });
   const classes = useStyles();
   console.log(posts);
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
