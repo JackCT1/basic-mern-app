36012483fc9bdabf3ad5955f40a04cfbcee1ba31
JackCT1 <jackthomas933@gmail.com>
2023-07-17 21:29:21 +0100

    button div

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -27,6 +27,11 @@ const Post = ({ post }) => {
           {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
     </Card>
   );
 };
